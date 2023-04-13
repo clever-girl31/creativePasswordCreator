579a9311e092444954b0dd9555003ca39d18b7b8
clever-girl31 <johnmseybold@gmail.com>
2023-04-12 17:02:24 -0700

    added comments

--- script1.js
+++ script1.js
@@ -1,29 +1,45 @@
-// Assignment Code
+// Password Generator
 
 
+// the four categories of character
 var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 var special = ['!', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '', ']', '^', '_', '`', '{', '|', '}', '~', '"', '']
+
+// variable to store desired password length
 var pwlength = []
+
+// empty array for storing character categories after user answers "Y". these become the possible ingredients for the pw.
 var ingredients = []
+
+// sends password output to text box
 var passwordOutput = document.querySelector("#password")
+
+// assign var to button for easy recall
 var generateBtn = document.querySelector("#generate");
 
+// did best to house entire password generator inside single giant parent function with multiple child functions
 function generatePassword() {
+  // first step is to learn user's desire password length
   getLength()
   
   function getLength() {
     pwlength = prompt("Enter desired character length:")
-    if (pwlength === null) {
+    // allows user to abort function if cancel is clicked
+    if (pwlength === null) { 
       return
     }
+    // checks if input meets requirements
     if (isNaN(pwlength) || pwlength < 8 || pwlength > 128) {
       alert("Please enter a numerical value between 8 and 128.")
       getLength()
-    } else UcYN()
-  }
+    // if requirements are met, next sub-function commences. if not, user is looped back to beginning of current subfunction.
+  } else UcYN()
+}
+  // the following three subfunctions follow same logical flow as getlength(), therefore comments will be fewer.
   
+  // allows user to select if uppercase character are wanted
   function UcYN() {
     let wantUc = prompt("Do you want uppercase letters in your password? Y/N")
     if (wantUc ===null) {
@@ -37,10 +53,12 @@ function generatePassword() {
       LcYN()
       return
     } else if (wantUc === 'Y' || wantUc ==='y') 
+    // sends uppercase bank to ingredients list before moving to next subfuction, if they are desired. if not, moves to next subfunction without adding to ingredients list.
     ingredients.push(...uppercase)
     LcYN()
   }
   
+  // user wants lowercase?
   function LcYN() {
     let wantLc = prompt("Do you want lowercase letters in your password? Y/N")
     if (wantLc === null) {
@@ -58,6 +76,7 @@ function generatePassword() {
     numYN()
   }
   
+  // user wants numbers?
   function numYN() {
     let wantNum = prompt("Do you want numbers in your password? Y/N")
     if (wantNum === null) {
@@ -75,6 +94,7 @@ function generatePassword() {
     specYN()
   }
   
+  // user wants special characters?
   function specYN() {
     let wantSpec = prompt("Do you want special characters in your password? Y/N")
     if (wantSpec === null) {
@@ -92,6 +112,7 @@ function generatePassword() {
     cook() 
   }
 
+  // !resumecommentaryhere
   function checker() {
     if (ingredients.length === 0) {
       alert("You must select at least one category.")
